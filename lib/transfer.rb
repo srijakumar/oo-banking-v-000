4679a8ea00cccfa7469d68cf7ccc603f67f9bce3
Srija S <srijakumar7@gmail.com>
2018-05-09 01:27:57 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -26,14 +26,17 @@ attr_reader :sender, :receiver
   end
 
   def reverse_transfer
-    if valid? && @sender.balance > amount && @status == "complete"
-      @sender.balance -= amount
-      @receiver.balance += amount
-      @status = "complete"
-    else
-      @status = "rejected"
-      "Transaction rejected. Please check your account balance."
+    if !execute_transaction
+      @status = "reversed"
     end
+    # if valid? && @sender.balance > amount && @status == "complete"
+    #   @receiver.balance -= amount
+    #   @sender.balance += amount
+    #   @status = "reversed"
+    # else
+    #   @status = "rejected"
+    #   "Transaction rejected. Please check your account balance."
+    # end
   end
 
 
